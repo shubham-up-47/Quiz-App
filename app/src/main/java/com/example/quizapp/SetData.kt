394441bc5b5886f71faf0d8c911ed438f43d4c08
Anuj Kumar Sharma <shubham271306@gmail.com>
2022-06-06 01:31:48 +0530

    Update

--- app/src/main/java/com/example/quizapp/SetData.kt
+++ app/src/main/java/com/example/quizapp/SetData.kt
@@ -11,51 +11,51 @@ object SetData
 
         var que1 = QuestionData(
             1,
-            "Pick the correct statement:",
-            "Mae Swapnil ke saath jaunga",
-            "Mae Vikas ke saath jaungi",
-            "Mae Swapnil ke sath jaungi",
-            "Mae Vikas ke saath jaunga",
-            3
+            "Which of the following are applications of Topological Sort of a graph?",
+            "Sentence Ordering",
+            "Course Scheduling",
+            "OS Deadlock Detection",
+            "All of the above",
+            4
         )
 
         var que2 = QuestionData(
             2,
-            "Yash's dream place:",
-            "Mumbai",
-            "America",
-            "Munnar",
-            "Noida",
-            4
+            "Maps in C++ are implemented using which of the following data structures?",
+            "Red Black Trees",
+            "Binary Search Trees",
+            "AVL Trees",
+            "Hash Tables",
+            1
         )
 
         var que3 = QuestionData(
             3,
-            "Class that changed Vikas's life:",
-            "Class 9th",
-            "Class 10th",
-            "Class 11th",
-            "Class 12th",
-            1
+            "What is the time complexity in precomputation & check prime of the Sieve of Eratosthenes algorithm?",
+            "O(n) & O(1)",
+            "O(nlog(logn)) & O(1)",
+            "O(nlogn) & O(1)",
+            "O(n) & O(logn)",
+            2
         )
 
         var que4 = QuestionData(
             1,
-            "IPL teams which Kiran hates & likes the most:",
-            "RR and MI",
-            "CSK and GT",
-            "RR and GT",
-            "MI and CSK",
+            "Which of the following algorithms are used for processing queries on trees?",
+            "Centroid Decomposition",
+            "Heavy Light Decomposition",
+            "All of the above",
+            "None of the above",
             3
         )
 
         var que5 = QuestionData(
             1,
-            "Vipul's favourite TA:",
-            "AS",
-            "AG",
-            "UM",
-            "RD",
+            "What is the space complexity in memorization & brute force (recursive) solution of the nth Fibonacci algorithm?",
+            "O(n) & O(2^n)",
+            "O(n) & O(n)",
+            "O(2^n) & O(2^n)",
+            "O(2^n) & O(n)",
             2
         )
 
